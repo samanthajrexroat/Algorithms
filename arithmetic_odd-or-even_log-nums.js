c6e4b7bb25cd2c657deff76a6ea2eaee3339b950
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-05-06 09:52:50 -0600

    Added log nums from 1 to num algo

--- arithmetic_odd-or-even_log-nums.js
+++ arithmetic_odd-or-even_log-nums.js
@@ -27,3 +27,13 @@ var oddOrEven = function(num) {
         return "odd"
     }
 };
+
+
+// Write code to print all numbers from 1 to `num`
+// Assume `num` will be a positive number
+
+var logNums = function(num) {
+    for ( var i = 1; i <= num; i++ ){
+        console.log(i)
+    }
+};
