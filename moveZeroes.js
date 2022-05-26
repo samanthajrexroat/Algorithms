ba8d8cc60fda99eb1503b32cc29bf673d622e6ae
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-05-26 12:53:16 -0600

    Changed to run backwards

--- moveZeroes.js
+++ moveZeroes.js
@@ -4,10 +4,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
  var moveZeroes = function(nums) {
-    for(let i = 0; i < nums.length; i++) {
+    for(let i = nums.length-1; i>=0 ; i--) {
         if (nums[i] === 0) {
-            nums.splice(i, 1);
             nums.push(0);
+            nums.splice(i, 1);
         }
     }
     return nums;
